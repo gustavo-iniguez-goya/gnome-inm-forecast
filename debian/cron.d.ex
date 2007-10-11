a4a6e623573fc8b4afe77120a289e3fe48fca408
ga <ga@b8f993d8-1228-4d1c-ab1b-4476b250cdba>
2007-10-11 10:17:08 +0000

    Moving files to trunk/

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the gnome-inm-forecast package
+#
+0 4	* * *	root	gnome-inm-forecast_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(gnome-inm-forecast):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="gnome-inm-forecast" command="/usr/bin/gnome-inm-forecast"

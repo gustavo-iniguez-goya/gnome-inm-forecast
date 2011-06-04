bf91b607c881c150afe1cab4ea554a66377c9589
ga <ga@b8f993d8-1228-4d1c-ab1b-4476b250cdba>
2011-06-04 16:37:21 +0000

    - Aemet has changed the URL of the localities weather forecast.

--- src/main.h
+++ src/main.h
@@ -44,7 +44,7 @@
 #define INM_URL "http://www.aemet.es"
 
 /*#define INM_FORECAST_URL "http://www.aemet.es/es/eltiempo/prediccion/localidades?l="*/
-#define INM_FORECAST_URL "http://www.aemet.es/es/eltiempo/prediccion/localidades/"
+#define INM_FORECAST_URL "http://www.aemet.es/es/eltiempo/prediccion/municipios/"
 #define INM_RADAR_AIR_MASS_IMG "http://www.aemet.es/imagenes_d/eltiempo/observacion/satelite/s91g"
 
 #define INM_RADAR_IMG "http://www.aemet.es/imagenes_d/eltiempo/observacion/radar/r8pb"
